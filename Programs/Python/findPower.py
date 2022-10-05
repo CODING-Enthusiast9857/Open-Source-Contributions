48caa942c5646767f6dd919307577c1ef8a09377
Ahzam Akhtar <ahzam8186@gmail.com>
2022-10-05 19:02:53 +0530

    PRogram To Find Power Added

---
+++ Programs/Python/findPower.py
@@ -0,0 +1,13 @@
+def power(x,n):
+  if n==0:
+    return 1
+  smallpower=power(x,n//2)
+  if n%2==0:
+    return smallpower*smallpower
+  else:
+    return x*smallpower*smallpower
+
+
+base = int(input("Enter the base"))
+exponent = int(input("Enter the exponent"))
+power(base,exponent)
