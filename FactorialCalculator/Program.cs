ede113b661490f49b1602938fdd278fbbf8dbb7b
7IronSnow7 <divan.debruin25@gmail.com>
2023-09-20 16:45:32 -0400

    This is a simple but effective factorial calculator.

---
+++ FactorialCalculator/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace FactorialCaculator
+{
+    // <summary>
+    /// Factorial Number
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Please enter a number to calculate its factorial:
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+        /// <summary>
+        /// Factorial Method finds the nth factorial for the desired input number.
+        /// </summary>
+        public static string Factorial(int number)
+        {
+            var factorialNum = 1;
+            // You can only calculate the factorial of positive numbers.
+            if (number > 0)
+            {
+                for (var i = 1; i <= number; i++)
+                {
+                    factorialNum *= i;
+                }
+                Console.Write($"Factorial of {number} is = ");
+            }
+            else if (number == 0) // Factorial of zero will always be one.
+            {
+                return "Factorial of 0 = 1";
+            }
+            else
+            {
+                return "Factorial is not defined for a negative number.";
+            }
+
+            return factorialNum.ToString();
+        }
+    }
+
+}
